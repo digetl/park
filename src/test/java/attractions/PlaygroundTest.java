8d5513604d25a6c0da83f4e4fc9407c24fe58a74
digetl <dave@digetl.co.uk>
2020-06-23 18:56:13 +0100

    isUnder15__True passed

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitorUnderAge;
+    Visitor visitorOverAge;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         playground = new Playground("Fun Zone", 7);
-    }
+        visitorUnderAge = new Visitor(14, 1.2, 40.0);
+        visitorOverAge = new Visitor(15, 1.6, 80.0);
+       }
 
     @Test
     public void hasName() {
@@ -26,5 +32,10 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void isUnder15__True() {
+        assertTrue(playground.isAllowedTo(visitorUnderAge));
     }
 }
