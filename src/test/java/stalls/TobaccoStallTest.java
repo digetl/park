816f389d4a912783ff9921ed25b46092972836b5
digetl <dave@digetl.co.uk>
2020-06-23 19:03:59 +0100

    check tobacco passed

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,23 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import people.Visitor;
+
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitorUnderAge;
+    Visitor visitorOverAge;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitorOverAge = new Visitor(18, 1.6, 80.0);
+        visitorUnderAge = new Visitor(17, 1.8, 80.0);
     }
 
     @Test
@@ -27,5 +34,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void checkIfUnder18() {
+        assertFalse(tobaccoStall.isAllowedTo(visitorUnderAge));
+    }
+
+    @Test
+    public void checkIsOver18() {
+        assertTrue(tobaccoStall.isAllowedTo(visitorOverAge));
     }
 }
